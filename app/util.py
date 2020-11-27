21ac9b2696f75475827a949ee9141aa51f3aa1db
Quinten <qvdpost@gmail.com>
2020-11-27 11:21:17 +0100

    added articles to lordnames

--- app/util.py
+++ app/util.py
@@ -18,16 +18,23 @@ def get_faction(prefs, data):
 
 
 def get_lord(faction, prefs, data):
-    lord = None
     count = 0
 
-    while lord is None and count < 50:
+    while count < 50:
         lord = random.choice(list(data.get(faction).get('lords').keys()))
+
         lord_info = data.get(faction).get('lords').get(lord)
+
+        if lord_info.get('article'):
+            lord = f"{lord_info.get('article')} {lord}"
+
         if lord_info.get('dlc'):
             dlc_name = lord_info.get('dlc_name') if lord_info.get('dlc_name') != "" else data.get(faction).get(
                 'dlc_name')
-            if dlc_name not in prefs['dlcs']:
-                lord = None
+            
+            if dlc_name in prefs['dlcs']:
+                return lord
+        else:
+            return lord
 
-    return lord
+    return None
